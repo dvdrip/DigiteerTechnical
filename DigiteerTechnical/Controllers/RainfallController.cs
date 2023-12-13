bddfc7ddf9d5e46270a48ba36bd16b8498bc55c7
David Barredo <dvdbarredo@gmail.com>
2023-12-13 18:52:55 +0800

    add sample data

--- DigiteerTechnical/Controllers/RainfallController.cs
+++ DigiteerTechnical/Controllers/RainfallController.cs
@@ -11,7 +11,40 @@ namespace DigiteerTechnical.Controllers
         [HttpGet("raifall/id/{stationId}/readings")]
         public IActionResult GetRainfallReadings(string stationId, [FromQuery] int count = 10)
         {
-            return Ok(new RainfallReadingResponse { Readings = new List<RainfallReading>() });
+            try
+            {
+                var readings = GenerateRainfallReadings(stationId, count);
+
+                return Ok(new RainfallReadingResponse { Readings = readings });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new ErrorResponse
+                {
+                    Message = "Internal server error",
+                    Detail = null
+                });
+            }
+        }
+
+        //Sample readings
+        private List<RainfallReading> GenerateRainfallReadings(string stationId, int count)
+        {
+            var readings = new List<RainfallReading>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var reading = new RainfallReading
+                {
+                    DateMeasured = DateTime.UtcNow.AddDays(-i),
+                    AmountMeasured = (decimal)new Random().NextDouble() * 100
+                };
+
+                readings.Add(reading);
+            }
+
+            return readings;
         }
     }
 }
